6354a0bc3499e5166fe00948d6051d5209dc8f16
mererakentaake <mererakentaake@gmail.com>
2026-04-29 05:21:35 +1100

    Delete preload.js

--- electron/preload.js
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require("electron");
-
-// Expose a safe API to the renderer process
-contextBridge.exposeInMainWorld("electronAPI", {
-  quit: () => ipcRenderer.send("app-quit"),
-  platform: process.platform,
-});
